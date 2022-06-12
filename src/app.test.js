49716800ebb431b0eddae54fe00b37955328fb5f
Xyza Rivera <xyzarivera@gmail.com>
2022-06-12 12:51:40 +0800

    add test for GET thumbnail endpoint

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,11 @@
 const request = require("supertest");
 const app = require("./app");
 const fs = require("fs");
+const config = require("./config");
 
 describe("/thumbnail", () => {
-  it.only("POST /thumbnails - should be able to upload a file", (done) => {
-    const testImagePath = "./test/assets/testImage.png";
+  it("POST /thumbnails - should be able to upload a file", (done) => {
+    const testImagePath = `${config.assetsDir}/testImage.png`;
     request(app)
       .post("/thumbnails")
       .attach("image", testImagePath)
@@ -19,8 +20,22 @@ describe("/thumbnail", () => {
       })
       .expect(() => {
         // eslint-disable-next-line no-undef
-        expect(fs.existsSync("./test/storage/testImage.png")).toBe(true);
-        fs.unlinkSync("./test/storage/testImage.png");
+        expect(fs.existsSync(`${config.storageDir}/testImage.png`)).toBe(true);
+        fs.unlinkSync(`${config.storageDir}/testImage.png`);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it("GET /thumbnails/:id - should be able to return the thumbnail given an id", (done) => {
+    request(app)
+      .get("/thumbnails/testImage.png")
+      .expect(200)
+      .expect("Content-Type", "image/png")
+      .expect(() => {
+        fs.unlinkSync(`${config.thumbnailsDir}/testImage.png`);
       })
       .end((err, res) => {
         if (err) return done(err);
