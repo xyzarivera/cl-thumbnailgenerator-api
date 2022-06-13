b44097c9b6fb58eec91c09e6850fa9e693338e6f
Xyza Rivera <xyzarivera@gmail.com>
2022-06-13 16:21:20 +0800

    add negative test scenario for  thumbnail generator

--- test/generator.test.js
+++ test/generator.test.js
@@ -3,24 +3,35 @@ const _thumbnailGenerator = require("../thumbnailGenerator/generator");
 const config = require("../src/config");
 
 describe("thumbnailGenerator", () => {
-  it("should be able resize an image to 100x100 pixels", () => {
-    const name = "testImage.png";
-    _thumbnailGenerator({
-      id: name,
-      imagePath: `${config.assetsDir}/${name}`,
-    });
+  // eslint-disable-next-line no-undef
+  afterAll(() => {
+    fs.unlink(`${config.thumbnailsDir}/testImage.png`, function (err) {});
+  });
+  it("should be able resize an image to 100x100 pixels", async () => {
+    const fileName = "testImage.png";
+    try {
+      await _thumbnailGenerator({
+        id: fileName,
+        imagePath: `./test/assets/${fileName}`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
     // eslint-disable-next-line no-undef
     expect(fs.existsSync(`${config.thumbnailsDir}/testImage.png`)).toBe(true);
   });
   it("should throw an error", async () => {
-    const name = "testImage.png";
+    const fileName = "testImage.png";
     try {
       await _thumbnailGenerator({
-        id: name,
-        imagePath: `${config.assetsDir}/${name}`,
+        id: fileName,
+        imagePath: `./test/testImage.png`,
       });
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      // eslint-disable-next-line no-undef
+      expect(error.message).toEqual(
+        "Input file is missing: ./test/testImage.png"
+      );
     }
   });
 });
